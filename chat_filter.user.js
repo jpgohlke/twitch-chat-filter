22097339e579b3dcf746defcfc5760a3f3181281
feha <felix@hallqvist.se>
2014-02-23 05:12:23 +0100

    Yet some GUI tweaks
    The width and padding of stuff looks kinda decent now.

--- chat_filter.user.js
+++ chat_filter.user.js
@@ -296,8 +296,8 @@ var initialize_ui = function(){
             "}" +
     
             ".segmented_tabs li li a.URLDuplicateFilterToggle {" +
-                "width: 48px;" +
-                "padding-left: 2px;" +
+                "width: 47px;" +
+                "padding-left: 3px;" +
                 "padding-top: 0;" +
                 "height: 8px;" +
                 "line-height: 115%;" +
@@ -329,10 +329,10 @@ var initialize_ui = function(){
     
     // Reduce the width of the chat button to fit the extra buttons we will add.
     var chat_button = $("ul.segmented_tabs li a").first();
-    chat_button.css("width", chat_button.width() - 80);
+    chat_button.css("width", chat_button.width() - 105);
 	
     var video_button = $("ul.segmented_tabs li a").last();
-    video_button.css("width", video_button.width() - 80);
+    video_button.css("width", video_button.width() - 95);
     
     // Add a couple of buttons to toggle the spam on and off and such
 	var newButons = "<li><a class='CommandsToggle'>Commands</a>" +
