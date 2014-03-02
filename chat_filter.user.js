fe57e0a8434765f2fc8d00c60a88bba26daadf1f
Hugo <hgualandi@inf.puc-rio.br>
2014-03-02 12:40:16 -0300

    Minor cleanup
    Fixed some names, removed a lot of mis-inserted whitespace

--- chat_filter.user.js
+++ chat_filter.user.js
@@ -2,16 +2,16 @@
 // @name        Twitch Plays Pokemon Chat Filter
 // @namespace   https://github.com/jpgohlke/twitch-chat-filter
 // @description Hide input commands from the chat.
- 
+
 // @include     http://www.twitch.tv/twitchplayspokemon
 // @include     http://www.twitch.tv/twitchplayspokemon/
 // @include     http://www.twitch.tv/chat/embed?channel=twitchplayspokemon&popout_chat=true
- 
-// @version     1.6
+
+// @version     1.7
 // @updateURL   http://jpgohlke.github.io/twitch-chat-filter/chat_filter.user.js
 // @grant       unsafeWindow
 // ==/UserScript==
- 
+
 /*
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,10 +19,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is furnished
  * to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
  * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
@@ -30,7 +30,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
- 
+
 /*
  * chat_filter.user.js
  *
@@ -45,7 +45,6 @@
  *     /u/DeathlyDeep
  *     /u/jeff_gohlke
  *     /u/yankjenets
- *     /u/hugomg
  *     /u/MKody
  *     /u/feha
  *     /u/jakery2
@@ -53,23 +52,23 @@
  *     /u/codefusion
  *     /u/Zephymastyx
  */
- 
+
 /* global unsafeWindow:false */
 /* jshint lastsemic:true */
- 
- 
+
+
 (function(){
 "use strict";
- 
+
 // --- Script configuration ---
- 
+
 var TPP_COMMANDS = [
     "left", "right", "up", "down",
     "start", "select",
     "a", "b",
     "democracy", "anarchy", "wait"
 ];
- 
+
 // Score-based filter for "Guys, we need to beat Misty" spam.
 var MISTY_SUBSTRINGS = [
     "misty",
@@ -78,7 +77,7 @@ var MISTY_SUBSTRINGS = [
     "we need to",
     "beat",
 ];
- 
+
 var URL_WHITELIST = [
     //us
      "github.com",
@@ -94,26 +93,26 @@ var URL_WHITELIST = [
     "strategywiki.org/wiki/Pok", //truncated before special characters
     "vgmaps.com"
 ];
- 
+
 var BANNED_WORDS = [
     "anus",
     "giveaway", "t-shirt", "hoodie",
     "imgur.com/4jlbxid.jpg"
 ];
- 
+
 var MINIMUM_DISTANCE_ERROR = 2; // Number of insertions / deletions / substitutions away from a blocked word.
 var MAXIMUM_NON_ASCII_CHARACTERS = 2; // For donger smilies, etc
 var MINIMUM_MESSAGE_WORDS = 2; // For Kappas and other short messages.
- 
+
 // The regexp Twitch uses to detect and automatically linkify URLs, with some modifications
 // so we can blacklist more messages.
 // - Recognizes *** in URLS (due to the Twitch chat censoring)
 // - Recognizes .mx and .sh TLDs
 var URL_REGEX = /\x02?((?:https?:\/\/|[\w\-\.\+]+@)?\x02?(?:[\w\-\*]+\x02?\.)+\x02?(?:com|au|org|tv|net|info|jp|uk|us|cn|fr|mobi|gov|co|ly|me|vg|eu|ca|fm|am|ws|mx|sh)\x02?(?:\:\d+)?\x02?(?:\/[\w\.\/@\?\&\%\#\(\)\,\-\+\=\;\:\x02?]+\x02?[\w\/@\?\&\%\#\(\)\=\;\x02?]|\x02?\w\x02?|\x02?)?\x02?)\x02?/g;
 var CENSORED_URL = /\*\*\*[\/\?\#\%]/g;
- 
+
 // --- Greasemonkey loading ---
- 
+
 //Greasemonkey userscripts run in a separate environment and cannot use
 //global variables from the page directly. We needd to access them via unsafeWindow
 var myWindow;
@@ -122,31 +121,31 @@ try{
 }catch(e){
     myWindow = window;
 }
- 
+
 var $ = myWindow.jQuery;
-var CurrentChat = null;
-   
+var chat_is_loaded = false;
+
 // --- Filtering predicates ---
- 
+
 // Adapted from https://gist.github.com/andrei-m/982927
 // Compute the edit distance between the two given strings
 function min_edit(a, b) {
-   
+
     if(a.length === 0) return b.length;
     if(b.length === 0) return a.length;
- 
+
     var matrix = [];
- 
+
     // increment along the first column of each row
     for(var i = 0; i <= b.length; i++) {
         matrix[i] = [i];
     }
- 
+
     // increment each column in the first row
     for(var j = 0; j <= a.length; j++) {
         matrix[0][j] = j;
     }
- 
+
     // Fill in the rest of the matrix
     for(var i = 1; i <= b.length; i++) {
         for(var j = 1; j <= a.length; j++) {
@@ -161,68 +160,68 @@ function min_edit(a, b) {
             }
         }
     }
- 
+
     return matrix[b.length][a.length];
 }
- 
+
 //This regex recognizes messages that contain exactly a chat command,
 //without any extra words around. This includes compound democracy mode
 //commands like `up2left4` and `start9`.
 // (remember to escape the backslashes when building a regexes from strings!)
 var compound_command_regex = new RegExp("^((" + TPP_COMMANDS.join("|") + ")\\d*)+$", "i");
- 
+
 function word_is_command(word){
- 
+
     if(compound_command_regex.test(word)) return true;
- 
+
     for(var j=0; j<TPP_COMMANDS.length; j++){
         var cmd = TPP_COMMANDS[j];
-         
+
         if(min_edit(cmd, word) <= MINIMUM_DISTANCE_ERROR){
            return true;
         }
     }
-    return false;  
+    return false;
 }
- 
+
 function message_is_command(message){
     message = message.toLowerCase();
-   
+
     var segments = message.split(/[\d\s]+/);
-   
+
     for(var i=0; i<segments.length; i++){
         var segment = segments[i];
         if(!segment) continue;
         if(!word_is_command(segment)) return false;
     }
-   
+
     return true;
 }
- 
- 
+
+
 function message_is_spam(message) {
     message = message.toLowerCase();
-   
+
     for(var i=0; i < BANNED_WORDS.length; i++){
         if(0 <= message.indexOf(BANNED_WORDS[i])){
             return true;
         }
     }
-   
+
     // Determine if message is variant of "Guys, we need to beat Misty."
     var misty_score = 0;
     for (var i = 0; i < MISTY_SUBSTRINGS.length; i++) {
         if (message.indexOf(MISTY_SUBSTRINGS[i]) != -1) {
             misty_score++;
-            if (misty_score > 1) {    
+            if (misty_score > 1) {
                 return true;
             }
         }
     }
-   
+
     return false;
 }
- 
+
 function is_whitelisted_url(url){
     //This doesnt actually parse the URLs but it
     //should do the job when it comes to filtering.
@@ -233,24 +232,24 @@ function is_whitelisted_url(url){
     }
     return false;
 }
- 
+
 function message_is_forbidden_link(message){
     message = message.toLowerCase();
- 
+
     if(CENSORED_URL.test(message)) return true;
- 
+
     var urls = message.match(URL_REGEX);
     if(!urls) return false;
-   
+
     for(var i=0; i<urls.length; i++){
         if(!is_whitelisted_url(urls[i])){
             return true;
         }
     }
-   
+
     return false;
 }
- 
+
 function message_is_donger(message){
     var nonASCII = 0;
     for(var i = 0; i < message.length; i++) {
@@ -263,16 +262,16 @@ function message_is_donger(message){
     }
     return false;
 }
- 
+
 function message_is_small(message){
     return message.split(/\s/g).length < MINIMUM_MESSAGE_WORDS;
 }
- 
+
 function message_is_cyrillic(message){
     //Some people use cyrillic characters to write spam that gets past the filter.
     return /[\u0400-\u04FF]/.test(message);
 }
- 
+
 function convert_copy_paste(message){
     //Replace repetitive text with only one instance of it
     //Useful for text and links where people do
@@ -280,9 +279,9 @@ function convert_copy_paste(message){
     //size of the message.
     return message.replace(/(.{4}.*?)(\s*?\1)+/g, "$1");
 }
- 
+
 // --- Filtering ---
- 
+
 //Filters have predicates that are called for every message
 //to determine whether it should get dropped or not
 var filters = [
@@ -291,49 +290,49 @@ var filters = [
     isActive: true,
     predicate: message_is_command
   },
- 
+
   { name: 'TppFilterLink',
     comment: "Non-whitelisted URLs",
     isActive: true,
     predicate: message_is_forbidden_link
   },
- 
+
   { name: 'TppFilterDonger',
     comment: "Ascii art and dongers",
     isActive: false,
     predicate: message_is_donger
   },
- 
+
   { name: 'TppFilterSmall',
     comment: "One-word messages",
     isActive: false,
     predicate: message_is_small
   },
- 
+
   { name: 'TppFilterSpam',
     comment: 'Spam',
     isActive: true,
     predicate: message_is_spam
   },
- 
+
   { name: 'TppFilterCyrillic',
     comment: 'Cyrillic characters',
     isActive: true,
     predicate: message_is_cyrillic
   }
 ];
- 
- 
+
+
 //Rewriters are applied to the text of a message
 //before it is inserted in the chat box
-var rewriters = [  
+var rewriters = [
   { name: 'TppFilterDuplicateURL',
     comment: "Copy pasted repetitions",
     isActive: true,
     rewriter: convert_copy_paste
   },
 ];
- 
+
 //Stylers are CSS classes that get toggled on/off
 var stylers = [
   { name: 'TppConvertAllcaps',
@@ -343,7 +342,7 @@ var stylers = [
     class: 'allcaps_filtered'
   },
 ];
- 
+
 function passes_active_filters(message){
     for(var i=0; i < filters.length; i++){
         var filter = filters[i];
@@ -354,7 +353,7 @@ function passes_active_filters(message){
     }
     return true;
 }
- 
+
 function rewrite_with_active_rewriters(message){
     var newMessage = message;
     for(var i=0;  i < rewriters.length; i++){
@@ -365,60 +364,59 @@ function rewrite_with_active_rewriters(message){
     }
     return newMessage;
 }
- 
+
 // --- UI ---
- 
+
 function initialize_ui(){
- 
+
     //TODO: #chat_line_list li.fromjtv
- 
+
     $("button.viewers").after('<a id="chat_filter_dropmenu_button" class="dropdown_glyph"><span></span><a>');
     $('#chat_filter_dropmenu_button').on('click', function(){
         $('#chat_filter_dropmenu').toggle();
     });
-       
-        $('#chat_filter_dropmenu_button span')
+
+    $('#chat_filter_dropmenu_button span')
         .css('background', 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAAv0lEQVQ4jc3SIQ7CQBAF0C8rK5E9AhI5R1gccpLOn+UACARHwCO5Aq6HQHAUQsAhwJGmlNBdIOEnY18mfwb4u4hIYWaSOySnAABVrWKMt9xx97OqVlDVkbufPoAuZiYAgBBC6e5NBnJQ1eqpK5KbBKQJIZQvyyc5f4eQ3A66pJlJjLG3N3dfJr0FyUUHudZ1PUtCWls9IDPbJyN90OBeulHV8beg6lfQKgsSkaJ18qOZTbIgAHD3NcmdiBTZSGruBIYOSjStwb0AAAAASUVORK5CYII=)')
         .css('position', 'relative');
-               
-        //Temporal fix to filter button style -- Begin
+
+    //Temporal fix to filter button style -- Begin
     $('#chat_filter_dropmenu_button')
         .css('background', '#c4c3c3')
-                .css('border', 'none')
-                .css('margin-left', '5px')
-                .css('padding', '4px 3px 2px')
-                .css('top', '10px');
-        $('.chat-buttons-container').css('top', '60px');
-        $('.send-chat-button').css('top', '10px');
-        //Temporal fix to filter button style -- End
-       
+        .css('border', 'none')
+        .css('margin-left', '5px')
+        .css('padding', '4px 3px 2px')
+        .css('top', '10px');
+    $('.chat-buttons-container').css('top', '60px');
+    $('.send-chat-button').css('top', '10px');
     $('.send-chat-button').css('left', '90px');
-        $('.chat-buttons-container').css('top', '60px');
+    //Temporal fix to filter button style -- End
+
     $('.chat-interface').append('<div id="chat_filter_dropmenu" class="dropmenu menu-like" style="position:absolute; bottom:45px; display:none;"><p style="margin-left:6px">Hide:</p></div>');
-   
-   
+
+
     var controlPanel = $('#chat_filter_dropmenu');
-   
+
     var customCssParts = [
         ".chat-messages .TppFiltered {display:none;} .filter_option{font-weight:normal; margin-bottom:0; color: #B9A3E3;}",
         "#chat.allcaps_filtered span.message{text-transform:lowercase;}"
     ];
- 
+
     $('head').append('<style>' + customCssParts.join("") + '</style>');
-   
+
     function add_option(option, update){
         controlPanel
         .append('<p class="dropmenu_action"><label for="' + option.name + '" class="filter_option"> <input type="checkbox" id="' + option.name + '">' + option.comment + '</label></p>');
- 
+
         $('#' + option.name)
         .on('change', function(){
             option.isActive = $(this).prop("checked");
             update(option);
-               
+
         })
         .prop('checked', option.isActive);
     }
-   
+
     filters.forEach(function(filter){
         add_option(filter, update_chat_with_filter);
     });
@@ -426,28 +424,30 @@ function initialize_ui(){
     rewriters.forEach(function(rewriter){
         add_option(rewriter, function(rewriter){});
     });
+
     function update_css(styler){
-                if(styler.isActive)
-                        $(styler.element).addClass(styler.class);
-                else
-                        $(styler.element).removeClass(styler.class);
+        if(styler.isActive){
+            $(styler.element).addClass(styler.class);
+        }else{
+            $(styler.element).removeClass(styler.class);
+        }
     }
     stylers.forEach(function(option){
         add_option(option, update_css);
         update_css(option);
     });
 }
- 
- 
+
+
 // --- Main ---
- 
+
 function update_chat_with_filter(){
-        if(!CurrentChat) return; //Chat hasnt loaded yet.
-       
+    if(chat_is_loaded) return;
+
     $('.chat-line').each(function() {
         var chatLine = $(this);
         var chatText = chatLine.find(".message").text().trim();
-       
+
         if(passes_active_filters(chatText)){
             chatLine.removeClass("TppFiltered");
         }else{
@@ -455,31 +455,31 @@ function update_chat_with_filter(){
         }
     });
 }
- 
+
 function initialize_filter(){
-    CurrentChat = myWindow.App.Room.prototype;
-   
     update_chat_with_filter();
-   
-    var original_insert_chat_line = CurrentChat.addMessage;
-    CurrentChat.addMessage = function(info) {
+
+    var Room_proto = myWindow.App.Room.prototype;
+    var original_insert_chat_line = Room_proto.addMessage;
+    Room_proto.addMessage = function(info) {
         if(!passes_active_filters(info.message)){ return false }
         info.message = rewrite_with_active_rewriters(info.message);
         return original_insert_chat_line.apply(this, arguments);
     };
 }
- 
+
 $(function(){
     //Checking for the spinner being gone is a more reliable way to chack
     //if the CurrentChat is fully loaded.
     var chatLoadedCheck = setInterval(function () {
-        if($(".loading-mask").length == 0){
+        if($(".loading-mask").length <= 0){
+            chat_is_loaded = true;
             clearInterval(chatLoadedCheck);
             initialize_ui();
-                        initialize_filter();
+            initialize_filter();
         }
     }, 100);
- 
+
 });
-   
+
 }());
