6731859e4abc52aaea2a11e748971b42845a6f0e
rctgamer3 <rctgamer3@hotmail.com>
2014-05-08 18:52:12 +0200

    Support for GreaseMonkey/Scriptish' meta update feature

---
+++ chat_filter.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Twitch Plays Pokemon Chat Filter
+// @namespace   https://github.com/jpgohlke/twitch-chat-filter
+// @description Hide input commands from the chat.
+
+// @include     /^https?://(www|beta)\.twitch\.tv\/(twitchplayspokemon(/(chat.*)?)?|chat\/.*channel=twitchplayspokemon.*)$/
+
+// @version     2.6
+// @updateURL   http://jpgohlke.github.io/twitch-chat-filter/chat_filter.meta.js
+// @downloadURL http://jpgohlke.github.io/twitch-chat-filter/chat_filter.user.js
+// @grant       unsafeWindow
+// ==/UserScript==
